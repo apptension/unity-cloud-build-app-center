076ac2c92c6ac25e29266fe906ab403cae7b0240
Krzysztof Skoracki <krzysztof.skoracki@unit9.com>
2018-06-27 11:00:13 +0200

    Add support for custom builds + use promises

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var options = {
     unityAPIBase: 'https://build-api.cloud.unity3d.com', // URI (e.g. href) recieved in web hook payload.
     unityCloudAPIKey: process.env.UNITYCLOUD_KEY,
     unityCloudSecret: process.env.UNITYCLOUD_SECRET,
-    hockeyappAPIUpload: 'https://rink.hockeyapp.net/api/2/apps/upload',
+    hockeyAppHost: 'rink.hockeyapp.net',
+    hockeyAppProtocol: 'https:',
     hockeyappAPIKey: process.env.HOCKEYAPP_KEY,
     logLevel: process.env.LOG_LEVEL || 0
 };
@@ -53,16 +54,14 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-// POST /api/users gets JSON bodies
-var mainRes;
 app.post('/build', jsonParser, function (req, res) {
     if (!req.body) {
         return res.sendStatus(400);
     }
 
-    mainRes = res;
+    console.log(req.body);
 
-    // 1. Get Build API URL
+    // Get Build API URL
     var buildAPIURL = req.body.links.api_self.href;
     if (!buildAPIURL) {
         // URL not available.
@@ -80,83 +79,160 @@ app.post('/build', jsonParser, function (req, res) {
         });
     }
 
-    // 2. Grab binary URL from Unity Cloud API
-    getBuildDetails(buildAPIURL, req.query.teams);
+    getBuildDetails(buildAPIURL)
+        .then(({ url, filename }) => downloadBinary(url, filename))
+        .then((filename) => uploadToHockeyApp(filename, req.body.platform, req.query.appId, req.query.teams));
 });
 
-function getBuildDetails (buildAPIURL, teams) {
-    console.log('1. getBuildDetails: start');
+function getBuildDetails (buildAPIURL) {
+    console.log('getBuildDetails: start');
 
-    najax({
-        url: options.unityAPIBase + buildAPIURL,
-        type: 'GET',
-        headers: {
-            'Authorization': 'Basic ' + options.unityCloudAPIKey
-        },
-        success: function (data) {
-            var parsedData = JSON.parse(data);
+    return new Promise((resolve, reject) =>
+        najax({
+            url: options.unityAPIBase + buildAPIURL,
+            type: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + options.unityCloudAPIKey
+            },
+            success: function (data) {
+                var parsedData = JSON.parse(data);
 
-            var parsedUrl = url.parse(parsedData.links.download_primary.href);
-            var filename = '/tmp/' + path.basename(parsedUrl.pathname);
+                var parsedUrl = url.parse(parsedData.links.download_primary.href);
+                var filename = '/tmp/' + path.basename(parsedUrl.pathname);
 
-            console.log('1. getBuildDetails: finished');
+                console.log('getBuildDetails: finished');
 
-            // 3. Download binary.
-            downloadBinary(parsedData.links.download_primary.href, filename, teams);
-        },
-        error: function (error) {
-            console.log(error);
-            mainRes.setHeader('Content-Type', 'application/json');
-            mainRes.send({
-                error: true,
-                message: 'Problem getting build details from Unity Cloud Build.',
-                errorDump: error
-            });
-        }
-    });
+                resolve({url: parsedData.links.download_primary.href, filename: filename});
+            },
+            error: function (error) {
+                console.log(error);
+                reject(error);
+            }
+        })
+    );
 }
 
-function downloadBinary (binaryURL, filename, teams) {
-    console.log('2. downloadBinary: start');
+function downloadBinary (binaryURL, filename) {
+    console.log('downloadBinary: start');
     console.log('   ' + binaryURL);
     console.log('   ' + filename);
 
-    deleteFile(filename);
+    return new Promise((resolve, reject) =>
+        deleteFile(filename, () =>
+            https.get(binaryURL, (res) => {
+                console.log('statusCode: ', res.statusCode);
+                console.log('headers: ', res.headers);
 
-    https.get(binaryURL, (res) => {
-        console.log('statusCode: ', res.statusCode);
-        console.log('headers: ', res.headers);
+                var writeStream = fs.createWriteStream(filename, {'flags': 'a'});
 
-        var writeStream = fs.createWriteStream(filename, {'flags': 'a'});
+                var len = parseInt(res.headers['content-length'], 10);
+                var cur = 0;
+                var total = len / 1048576; // 1048576 - bytes in  1Megabyte
 
-        var len = parseInt(res.headers['content-length'], 10);
-        var cur = 0;
-        var total = len / 1048576; // 1048576 - bytes in  1Megabyte
+                res.on('data', (chunk) => {
+                    cur += chunk.length;
+                    writeStream.write(chunk, 'binary');
 
-        res.on('data', (chunk) => {
-            cur += chunk.length;
-            writeStream.write(chunk, 'binary');
+                    if (options.logLevel >= 1) {
+                        console.log('Downloading ' + (100.0 * cur / len).toFixed(2) + '%, Downloaded: ' + (cur / 1048576).toFixed(2) + ' mb, Total: ' + total.toFixed(2) + ' mb');
+                    }
+                });
 
-            if (options.logLevel >= 1) {
-                console.log('Downloading ' + (100.0 * cur / len).toFixed(2) + '%, Downloaded: ' + (cur / 1048576).toFixed(2) + ' mb, Total: ' + total.toFixed(2) + ' mb');
-            }
-        });
+                res.on('end', () => {
+                    console.log('downloadBinary: finished');
+                    writeStream.end();
+                });
 
-        res.on('end', () => {
-            console.log('2. downloadBinary: finished');
-            writeStream.end();
-        });
-
-        writeStream.on('finish', () => {
-            uploadToHockeyApp(filename, teams);
-        });
-    }).on('error', (e) => {
-        console.error(e);
-    });
+                writeStream.on('finish', () => {
+                    resolve(filename);
+                });
+            }).on('error', (e) => {
+                console.error(e);
+                reject(e);
+            })
+        )
+    );
 }
 
-function uploadToHockeyApp (filename, teams) {
-    console.log('3. uploadToHockeyApp: start');
+function uploadToHockeyApp (filename, platform, appId, teams) {
+    if (platform === 'android' || platform === 'ios') {
+        return uploadFileToHockeyApp(filename, teams);
+    } else {
+        return getHockeyAppVersion(appId)
+            .then((version) => createHockeyAppVersion(appId, version + 1))
+            .then((versionId) => updateHockeyAppVersion(appId, versionId, filename, teams));
+    }
+}
+
+function getHockeyAppVersion (appId) {
+    console.log('getHockeyAppVersion: start');
+    var url = options.hockeyAppProtocol + '//' + options.hockeyAppHost + '/api/2/apps/' + appId + '/app_versions';
+
+    return new Promise((resolve, reject) =>
+        najax({
+            url: url,
+            type: 'GET',
+            headers: {
+                'X-HockeyAppToken': options.hockeyappAPIKey
+            },
+            success: function (data) {
+                var parsedData = JSON.parse(data);
+
+                var version = 0;
+                if (parsedData.app_versions.length > 0) {
+                    version = parseInt(parsedData.app_versions[0].version);
+                }
+
+                console.log('getHockeyAppVersion: finished');
+                resolve(version);
+            },
+            error: function (error) {
+                console.log(error);
+                reject(error);
+            }
+        })
+    );
+}
+
+function createHockeyAppVersion (appId, version) {
+    console.log('createHockeyAppVersion: start');
+    // HockeyApp properties
+    var HOCKEY_APP_PATH = '/api/2/apps/' + appId + '/app_versions/new';
+
+    // Create FormData
+    var form = new FormData();
+    form.append('bundle_version', version);
+
+    return new Promise((resolve, reject) =>
+        form.submit({
+            host: options.hockeyAppHost,
+            path: HOCKEY_APP_PATH,
+            protocol: options.hockeyAppProtocol,
+            headers: {
+                'Accept': 'application/json',
+                'X-HockeyAppToken': options.hockeyappAPIKey
+            }
+        }, function (err, res) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+
+            var body = '';
+
+            res.on('data', (chunk) => { body += chunk; });
+
+            res.on('end', () => {
+                var parsedData = JSON.parse(body);
+                console.log('createHockeyAppVersion: finished');
+                resolve(parsedData.id);
+            });
+        })
+    );
+}
+
+function updateHockeyAppVersion (appId, versionId, filename, teams) {
+    console.log('updateHockeyAppVersion: start');
 
     var readable = fs.createReadStream(filename);
     readable.on('error', () => {
@@ -164,9 +240,62 @@ function uploadToHockeyApp (filename, teams) {
     });
 
     // HockeyApp properties
-    var HOCKEY_APP_HOST = 'rink.hockeyapp.net';
+    var HOCKEY_APP_PATH = '/api/2/apps/' + appId + '/app_versions/' + versionId;
+
+    // Create FormData
+    var form = new FormData();
+    form.append('status', 2);
+    form.append('ipa', readable);
+
+    if (teams) {
+        form.append('teams', teams);
+    }
+
+    return new Promise((resolve, reject) =>
+        form.submit({
+            host: options.hockeyAppHost,
+            path: HOCKEY_APP_PATH,
+            protocol: options.hockeyAppProtocol,
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'X-HockeyAppToken': options.hockeyappAPIKey
+            }
+        }, function (err, res) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+
+            if (res.statusCode !== 200 && res.statusCode !== 201) {
+                console.log('Uploading failed with status ' + res.statusCode);
+                console.log(res);
+                reject(err);
+            }
+
+            var jsonString = ''; // eslint-disable-line
+            res.on('data', (chunk) => {
+                jsonString += String.fromCharCode.apply(null, new Uint16Array(chunk));
+            });
+
+            res.on('end', () => {
+                console.log('updateHockeyAppVersion: finished');
+                deleteFile(filename, resolve);
+            });
+        })
+    );
+}
+
+function uploadFileToHockeyApp (filename, teams) {
+    console.log('uploadToHockeyApp: start');
+
+    var readable = fs.createReadStream(filename);
+    readable.on('error', () => {
+        console.log('Error reading binary file for upload to HockeyApp');
+    });
+
+    // HockeyApp properties
     var HOCKEY_APP_PATH = '/api/2/apps/upload/';
-    var HOCKEY_APP_PROTOCOL = 'https:';
 
     // Create FormData
     var form = new FormData();
@@ -181,53 +310,54 @@ function uploadToHockeyApp (filename, teams) {
         form.append('teams', teams);
     }
 
-    var req = form.submit({
-        host: HOCKEY_APP_HOST,
-        path: HOCKEY_APP_PATH,
-        protocol: HOCKEY_APP_PROTOCOL,
-        headers: {
-            'Accept': 'application/json',
-            'X-HockeyAppToken': options.hockeyappAPIKey
-        }
-    }, function (err, res) {
-        if (err) {
-            console.log(err);
-        }
+    return new Promise((resolve, reject) => {
+        var req = form.submit({
+            host: options.hockeyAppHost,
+            path: HOCKEY_APP_PATH,
+            protocol: options.hockeyAppProtocol,
+            headers: {
+                'Accept': 'application/json',
+                'X-HockeyAppToken': options.hockeyappAPIKey
+            }
+        }, function (err, res) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
 
-        if (res.statusCode !== 200 && res.statusCode !== 201) {
-            console.log('Uploading failed with status ' + res.statusCode);
-            console.log(res);
+            if (res.statusCode !== 200 && res.statusCode !== 201) {
+                console.log('Uploading failed with status ' + res.statusCode);
+                reject(err);
+            }
 
-            return;
-        }
+            var jsonString = ''; // eslint-disable-line
+            res.on('data', (chunk) => {
+                jsonString += String.fromCharCode.apply(null, new Uint16Array(chunk));
+            });
 
-        var jsonString = ''; // eslint-disable-line
-        res.on('data', (chunk) => {
-            jsonString += String.fromCharCode.apply(null, new Uint16Array(chunk));
+            res.on('end', () => {
+                console.log('uploadToHockeyApp: finished');
+
+                deleteFile(filename, resolve);
+            });
         });
 
-        res.on('end', () => {
-            console.log('3. uploadToHockeyApp: finished');
+        // Track upload progress.
+        var len = parseInt(req.getHeader('content-length'), 10);
+        var cur = 0;
+        var total = len / 1048576; // 1048576 - bytes in  1Megabyte
 
-            deleteFile(filename);
+        req.on('data', (chunk) => {
+            cur += chunk.length;
+            if (options.logLevel >= 1) {
+                console.log('Downloading ' + (100.0 * cur / len).toFixed(2) + '%, Downloaded: ' + (cur / 1048576).toFixed(2) + ' mb, Total: ' + total.toFixed(2) + ' mb');
+            }
         });
-    });
-
-    // Track upload progress.
-    var len = parseInt(req.getHeader('content-length'), 10);
-    var cur = 0;
-    var total = len / 1048576; // 1048576 - bytes in  1Megabyte
-
-    req.on('data', (chunk) => {
-        cur += chunk.length;
-        if (options.logLevel >= 1) {
-            console.log('Downloading ' + (100.0 * cur / len).toFixed(2) + '%, Downloaded: ' + (cur / 1048576).toFixed(2) + ' mb, Total: ' + total.toFixed(2) + ' mb');
-        }
     });
 }
 
 // Delete file, used to clear up any binary downloaded.
-function deleteFile (filename) {
+function deleteFile (filename, cb) {
     fs.access(filename, function (err) {
         if (!err || err.code !== 'ENOENT') {
             // Delete File.
@@ -235,7 +365,11 @@ function deleteFile (filename) {
                 if (err) {
                     console.log(err);
                 }
+
+                cb();
             });
+        } else {
+            cb();
         }
     });
 }
